89e1f75527011ddc4f6a4903e594db4ac7f138c9
Roberto Carlos Leonardo <roberto.c.leonardo@gmail.com>
2020-08-24 18:36:44 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,11 +8,31 @@ $usuarios = $sql-> select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
+
+/*
 $root = new Usuario();
-
 $root->loadbyId(3);
-
 echo $root;
+*/
+
+/* getList é statico, então nem precisa instanciar o objeto usuario...
+// chama direto a função.
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/* search é statico, então nem precisa instanciar o objeto usuario...
+// chama direto a função.
+$search = Usuario::search("ser");
+echo json_encode($search);
+*/
+
+// carrega um usuario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("user","12345");
+echo $usuario;
+
 ?>
 
 
